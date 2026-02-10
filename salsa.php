07aefd3511bb821512a55a68d4188e7e704225fc
AlaaSall <salsabilamobile636@gmail.com>
2026-02-10 18:33:17 +0700

    Add initial HTML structure in salsa.php

---
+++ salsa.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 1</title>
+</head>
+<body>
+    <p>Assalamualaikum</p>
+    <h5>bab 1</h5>
+    <br>
+    <h2>HTML Dasar</h2>
+</body>
+</html>
